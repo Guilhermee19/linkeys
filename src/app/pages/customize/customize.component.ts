7c8a350015cffe388e43e125f84b6b2f78e5a29d
Guilhermee19 <guilherme@noclaftech.com>
2024-06-13 18:42:42 -0300

    feat: ajuste

--- src/app/pages/customize/customize.component.ts
+++ src/app/pages/customize/customize.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LinksComponent } from '../../components/customize/links/links.component';
+import { ProfileComponent } from '../../components/customize/profile/profile.component';
 
 @Component({
   selector: 'app-customize',
   standalone: true,
-  imports: [],
+  imports: [LinksComponent, ProfileComponent],
   templateUrl: './customize.component.html',
   styleUrl: './customize.component.scss'
 })
